ea7bb1eba8e3181fec8cf59439430ad8427b78b5
developerbola <developerbola08@gmail.com>
2025-05-18 22:56:42 +0500

    #17 Build home page, add custom break points, do navbar background more darker

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,7 +1,35 @@
-import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
-const page = () => {
-  return <div className="">Home</div>;
-};
+interface Image {
+  id: string;
+  author: string;
+  width: number;
+  height: number;
+  url: string;
+  download_url: string;
+}
 
-export default page;
+export default async function Home() {
+  const res = await fetch(`https://picsum.photos/v2/list?page=1&limit=100`);
+  const data = await res.json();
+
+  return (
+    <div className="w-full exs:columns-2 md:columns-3 lg:columns-4 gap-5">
+      {data.map((image: Image) => (
+        <Link href={`/${image.id}`} key={image.id}>
+          <div className="mb-5 break-inside-avoid flex flex-col gap-2">
+            <Image
+              src={image.download_url}
+              height={image.height}
+              width={image.width}
+              alt={image.author}
+              className="w-auto h-auto rounded-lg"
+            />
+            <h2>{image.author}</h2>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+}
