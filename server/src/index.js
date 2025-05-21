05b4ca38edea9a8e6922d34bc92da64260528623
developerbola <developerbola08@gmail.com>
2025-05-21 13:56:51 +0500

    #36 Vercel fix

---
+++ server/src/index.js
@@ -0,0 +1,10 @@
+import { Hono } from "hono";
+import { useCors } from "./middlewares/corsMiddleware.js";
+import { useRoutes } from "./routes/routes.js";
+
+const app = new Hono();
+
+useCors(app);
+useRoutes(app);
+
+export default app.fetch;
