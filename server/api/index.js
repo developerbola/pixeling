b66e6a1016810220d52cdfdaedadde6a0b5f946f
developerbola <developerbola08@gmail.com>
2025-05-22 14:06:14 +0500

    #53 Testing 2 vercel bug

--- server/api/index.js
+++ server/api/index.js
@@ -1,7 +1,11 @@
 import { Hono } from "hono";
+import { useCors } from "../src/middlewares/corsMiddleware.js";
 import { handle } from "hono/vercel";
 
 const app = new Hono().basePath("/api");
+
+// Add CORS middleware
+useCors(app);
 
 app.get("/", (c) => {
   return c.json({ message: "Hello from Hono on Vercel!" });
