ae67460d4fdd230f5c4ce4b322e8af321e09a478
developerbola <developerbola08@gmail.com>
2025-05-22 13:40:37 +0500

    #43 Fix backend vercel configuration

--- server/src/index.js
+++ server/src/index.js
@@ -4,12 +4,14 @@ import { useRoutes } from "./routes/routes.js";
 import { handle } from "hono/vercel";
 
 const app = new Hono().basePath("/api");
-export const runtime = 'nodejs'
 
 useCors(app);
 useRoutes(app);
 
-export const GET = handle(app);
-export const POST = handle(app);
-export const PUT = handle(app);
-export const DELETE = handle(app);
+const handler = handle(app);
+
+export const GET = handler;
+export const POST = handler;
+export const PATCH = handler;
+export const PUT = handler;
+export const OPTIONS = handler;
