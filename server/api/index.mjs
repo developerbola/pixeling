e542d46135136f2fb50f98cc173537426a181bd5
developerbola <developerbola08@gmail.com>
2025-05-23 07:12:22 +0500

    Improve

--- server/api/index.mjs
+++ server/api/index.mjs
@@ -3,19 +3,19 @@ dotenv.config();
 
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
-// import { useCors } from "../src/middlewares/corsMiddleware.js";
-// import { useRoutes } from "../src/routes/useRoutes.js";
+import { useCors } from "../src/middlewares/corsMiddleware.js";
+import { useRoutes } from "../src/routes/useRoutes.js";
 
 // Initialize app and apply middlewares
 const app = new Hono();
-// useCors(app);
-// useRoutes(app);
+useCors(app);
+useRoutes(app);
 app.get("/", (c) => {
   c.json({ message: "hello" });
 });
 
 // Export the Vercel handler and HTTP method handlers
-export default handle(app);
+export default app;
 export const GET = handle(app);
 export const POST = handle(app);
 export const PUT = handle(app);
