76374b2dad6c46336f08728d90bf3f2c49662ce0
developerbola <developerbola08@gmail.com>
2025-05-27 17:52:34 +0500

    Fix cors for host

--- server/src/middlewares/corsMiddleware.js
+++ server/src/middlewares/corsMiddleware.js
@@ -1,7 +1,14 @@
 const { cors } = require("hono/cors");
 
 const useCors = (app) => {
-  app.use(cors());
+  app.use(
+    "*",
+    cors({
+      origin: "https://pixeling.vercel.app",
+      allowMethods: ["GET", "POST", "DELETE", "OPTIONS"],
+      allowHeaders: ["Content-Type", "Authorization"],
+    })
+  );
 };
 
 module.exports = { useCors };
