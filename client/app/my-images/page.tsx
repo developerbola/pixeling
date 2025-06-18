a9c7c46afefaf77bbf69bb01ea3888b7ae361419
developerbola <developerbola08@gmail.com>
2025-06-18 09:55:59 +0500

    Wrap user images fetch in useEffect

--- client/app/my-images/page.tsx
+++ client/app/my-images/page.tsx
@@ -1,43 +1,55 @@
-"use client"
+"use client";
 import Link from "next/link";
 import { ImageType } from "../page";
 import ImageItem from "@/components/ImageItem";
 import { useAtomValue } from "jotai";
 import { userAtom } from "@/lib/atom";
+import { useEffect } from "react";
 
-const MyImages = async () => {
+const MyImages = () => {
   let images: ImageType[] = [];
   const user = useAtomValue(userAtom);
 
-  try {
-    const res = await fetch(
-      `${process.env.NEXT_PUBLIC_BACKEND_URL}/${user?.id as string}/images`,
-      {
-        cache: "no-cache",
+  useEffect(() => {
+    const fetchImages = async () => {
+      try {
+        const res = await fetch(
+          `${process.env.NEXT_PUBLIC_BACKEND_URL}/${user?.id as string}/images`,
+          {
+            cache: "no-cache",
+          }
+        );
+        images = await res.json();
+      } catch (error) {
+        console.error("Fetch error:", error);
+        return (
+          <div className="grid place-items-center h-[calc(90vh-80px)]">
+            <h1 className="text-2xl">
+              Unable to connect. You or server might be offline.
+            </h1>
+          </div>
+        );
       }
-    );
-    images = await res.json();
-  } catch (error) {
-    console.error("Fetch error:", error);
-    return (
-      <div className="grid place-items-center h-[calc(90vh-80px)]">
-        <h1 className="text-2xl">
-          Unable to connect. You or server might be offline.
-        </h1>
-      </div>
-    );
-  }
+    };
+    fetchImages();
+  }, [user?.id]);
 
   return (
     <div className="w-full exs:columns-2 md:columns-3 lg:columns-4 exs:gap-2 sm:gap-5">
-      {images.map((image: ImageType) => (
-        <Link href={`/image/${image.id}`} key={image.id}>
-          <div className="exs:mb-3 sm:mb-5 break-inside-avoid flex flex-col gap-2">
-            <ImageItem image={image} />
-            <h2>{image.title}</h2>
-          </div>
-        </Link>
-      ))}
+      {images.length == 0 ? (
+        <>
+          <div className="w-full text-center">No images yet</div>
+        </>
+      ) : (
+        images.map((image: ImageType) => (
+          <Link href={`/image/${image.id}`} key={image.id}>
+            <div className="exs:mb-3 sm:mb-5 break-inside-avoid flex flex-col gap-2">
+              <ImageItem image={image} />
+              <h2>{image.title}</h2>
+            </div>
+          </Link>
+        ))
+      )}
     </div>
   );
 };
