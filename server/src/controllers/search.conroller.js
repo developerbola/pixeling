aa6c5e946a2afe3ff987bf24ec1d3bab7e9b3498
developerbola <developerbola08@gmail.com>
2025-06-21 12:39:18 +0500

    Build serach controller for usage

--- server/src/controllers/search.conroller.js
+++ server/src/controllers/search.conroller.js
@@ -1,11 +1,22 @@
-const search = (c) => {
+import { supabase } from "../config/supabase.js";
+
+const search = async (c) => {
   try {
-    return c.json({ message: "/search route working" }, 200);
+    const { searchPrompt } = await c.req.json();
+    const { data, error } = await supabase
+      .from("image-list")
+      .select("name, description")
+      .ilike("name", `%${searchPrompt}%`);
+
+    if (error) {
+      return c.json({ error: "An error occurred during searching" }, 500);
+    }
+
+    return c.json(data);
   } catch (error) {
     return c.json(
       {
-        error:
-          error || error.error || error.message || "Unexpected error occured",
+        error: error?.message || error?.error || "Unexpected error occurred",
       },
       500
     );
