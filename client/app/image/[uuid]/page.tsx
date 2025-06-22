a3180637a36b2923341708fee0b09896fbe48520
developerbola <developerbola08@gmail.com>
2025-06-22 15:32:14 +0500

    Making single image page in client side

--- client/app/image/[uuid]/page.tsx
+++ client/app/image/[uuid]/page.tsx
@@ -1,11 +1,15 @@
 "use client";
 import { ImageType } from "@/app/page";
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 import { userAtom } from "@/lib/atom";
+import { cn } from "@/lib/utils";
 import { useAtomValue } from "jotai";
+import { LoaderCircle } from "lucide-react";
+import Image from "next/image";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
-const Image = () => {
+const SingleImage = () => {
   const { uuid } = useParams();
   const user = useAtomValue(userAtom);
   const [image, setImage] = useState<ImageType>({
@@ -21,6 +25,7 @@ const Image = () => {
     categories: [],
     author_uuid: "",
   });
+  const [loaded, setLoaded] = useState<boolean>(false);
 
   useEffect(() => {
     const fetchImage = async () => {
@@ -34,18 +39,54 @@ const Image = () => {
   }, []);
 
   return (
-    <div>
-      <h1>Image: {uuid}</h1>
-      <h1>
-        isOwner:{" "}
-        {image && user
-          ? image.author_uuid === user.id
-            ? "Yes"
-            : "No"
-          : "Checking..."}
-      </h1>
+    <div className="w-full h-full flex justify-center">
+      <div className="flex flex-col gap-3 w-[70%]">
+        <div className="h-[400px]">
+          {image.imageUrl && (
+            <div
+              className="rounded-lg overflow-hidden"
+              style={{
+                backgroundColor: loaded ? "transparent" : image.dominantColor,
+              }}
+            >
+              <Image
+                src={image.imageUrl}
+                alt={(image.title, image.description)}
+                width={400}
+                height={400}
+                onLoad={() => setLoaded(true)}
+                decoding="async"
+                loading="lazy"
+                className={cn(
+                  `transition-opacity duration-500 w-auto h-[400px]`,
+                  loaded ? "opacity-100" : "opacity-0"
+                )}
+              />
+            </div>
+          )}
+        </div>
+        <div>
+          <h1 className="text-3xl font-bold">{image.title}</h1>
+          <h1 className="text-gray-400 text-lg">
+            {image.description}
+          </h1>
+        </div>
+        <div className="flex items-center gap-2">
+          <Avatar className="cursor-pointer border-[0.5px] border-[#ffffff40]">
+            <AvatarImage src={user?.user_metadata?.avatar_url} />
+            <AvatarFallback>
+              <LoaderCircle className="animate-spin" />
+            </AvatarFallback>
+          </Avatar>
+          <h1>{user?.user_metadata?.name}</h1>
+        </div>
+        <div>
+          <h1>Comments</h1>
+          <div></div>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default Image;
+export default SingleImage;
