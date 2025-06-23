283418ae05c3c2c93f820a4a0f9e031f7a2f58ab
developerbola <developerbola08@gmail.com>
2025-06-23 15:22:37 +0500

    Doing main page ssr fetch

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,11 +1,6 @@
-"use client";
-
 import ImageItem from "@/components/ImageItem";
-import { imagesAtom } from "@/lib/atom";
-import { useAtomValue, useSetAtom } from "jotai";
 import { LoaderCircle } from "lucide-react";
 import Link from "next/link";
-import { useEffect, useState } from "react";
 
 export interface ImageType {
   id: string;
@@ -23,31 +18,11 @@ export interface ImageType {
 
 export type ImagesListType = { code: number; message: string } | ImageType[];
 
-export default function Home() {
-  const setImagesList = useSetAtom(imagesAtom);
-  const imagesList = useAtomValue(imagesAtom);
+export default async function Home() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/images`);
+  const data = await res.json();
 
-  useEffect(() => {
-    const fetchImages = async () => {
-      try {
-        const res = await fetch(
-          `${process.env.NEXT_PUBLIC_BACKEND_URL}/images`
-        );
-        const data = await res.json();
-        setImagesList(data);
-      } catch (error) {
-        setImagesList({
-          code: 500,
-          message: `Failed to fetch images: ${error}`,
-        });
-        console.log(error);
-      }
-    };
-
-    fetchImages();
-  }, [imagesList]);
-
-  if (imagesList === null) {
+  if (data === null) {
     return (
       <div className="grid place-items-center h-[calc(90vh-150px)] text-[#ffffff90]">
         <LoaderCircle className="animate-spin" size={32} />
@@ -56,10 +31,10 @@ export default function Home() {
   }
 
   // Render error state
-  if (imagesList && !Array.isArray(imagesList)) {
+  if (data && !Array.isArray(data)) {
     return (
       <div className="grid place-items-center h-[calc(90vh-150px)] text-[#ffffff90]">
-        <h2 className="text-2xl">{imagesList.message}</h2>
+        <h2 className="text-2xl">{data.message}</h2>
       </div>
     );
   }
@@ -67,7 +42,7 @@ export default function Home() {
   // Render images
   return (
     <div className="w-full exs:columns-2 md:columns-3 lg:columns-4 exs:gap-2 sm:gap-5">
-      {imagesList?.map((image: ImageType) => (
+      {data?.map((image: ImageType) => (
         <Link href={`/image/${image.id}`} key={image.id}>
           <div className="exs:mb-3 sm:mb-5 break-inside-avoid flex flex-col gap-2">
             <ImageItem image={image} />
--- client/app/user/[username]/page.tsx
+++ client/app/user/[username]/page.tsx
@@ -1,10 +1,30 @@
 "use client";
 
 import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const User = () => {
+  const [user, setUser] = useState([]);
   const { username } = useParams();
-  return <div>User: {username}</div>;
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const userRes = await fetch(
+          `${process.env.NEXT_PUBLIC_BACKEND_URL}/user/username/${username}`
+        );
+        const userData = await userRes.json();
+        setUser(userData);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchUser();
+  }, []);
+  return (
+    <div>
+      User: <code>{JSON.stringify(user)}</code>
+    </div>
+  );
 };
 
 export default User;
