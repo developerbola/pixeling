2a63071e2373122a8522dc13a652b781236e9b0b
developerbola <developerbola08@gmail.com>
2025-07-14 10:48:26 +0500

    Enhance comments functionality: add comment submission logic and loading states

--- client/components/Comments.tsx
+++ client/components/Comments.tsx
@@ -1,12 +1,15 @@
 "use client";
 
 import { ImageType } from "@/app/page";
-import { Loader, Loader2, LoaderCircle, PlusCircle } from "lucide-react";
+import { Delete, LoaderCircle, PlusCircle, Trash } from "lucide-react";
 import { useEffect, useState } from "react";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import Link from "next/link";
+import { useAtomValue } from "jotai";
+import { userAtom } from "@/lib/atom";
+import { toast } from "sonner";
 
 type Comment = {
   image_uuid: string;
@@ -20,6 +23,120 @@ type Error = {
   error?: string;
 };
 
+const CommentsLogic = ({ image }: { image: ImageType }) => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [comments, setComments] = useState<Comment[] | Error>([]);
+  const [newComment, setNewComment] = useState<string>("");
+
+  const user = useAtomValue(userAtom);
+  const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/comments`;
+
+  useEffect(() => {
+    const fetchComments = async () => {
+      setIsLoading(true);
+      const res = await fetch(`${url}/${image.id}`, { cache: "no-store" });
+      const data = await res.json();
+      setComments(data);
+      setIsLoading(false);
+    };
+    fetchComments();
+  }, [image.id]);
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const _new = {
+      image_uuid: image.id,
+      author_uuid: user?.id ?? "",
+      author_name: user?.user_metadata?.name || "",
+      author_avatar_url: user?.user_metadata?.avatar_url,
+      comment: newComment,
+    };
+    const commentAddLink = `http://localhost:3000/api/comments/add`;
+    try {
+      const res = await fetch(commentAddLink, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(_new),
+      });
+      console.log(res);
+
+      const data = await res.json();
+      console.log(data);
+
+      if (!res.ok) {
+        toast.error(data.error || "Something went wrong!");
+        return;
+      }
+
+      toast.success("Comment added successfully!");
+
+      setComments([...(Array.isArray(comments) ? comments : []), _new]);
+      setNewComment("");
+    } catch (error) {
+      toast.error("Something went wrong!");
+      console.error(error);
+    }
+  };
+
+  if (isLoading) {
+    return <div className="w-full h-[50px] animate-pulse bg-[#ffffff20]" />;
+  }
+
+  if ("error" in comments) {
+    return <div>{comments.error}</div>;
+  }
+
+  return (
+    <div className="flex flex-col gap-3">
+      {Array.isArray(comments) ? (
+        comments.length === 0 && !isLoading ? (
+          <div className="text-gray-500">No comments yet, be first!</div>
+        ) : (
+          <div className="flex flex-col gap-2">
+            {comments.map((comment: Comment) => {
+              return (
+                <div
+                  key={comment.author_uuid + comment.comment}
+                  className="flex flex-wrap gap-2"
+                >
+                  <Link
+                    href={`/user/${comment.author_uuid}`}
+                    className="flex items-center gap-1"
+                  >
+                    <Avatar className="cursor-pointer border-[0.5px] border-[#ffffff40] size-5">
+                      <AvatarImage src={comment.author_avatar_url} />
+                      <AvatarFallback>
+                        <LoaderCircle className="animate-spin" />
+                      </AvatarFallback>
+                    </Avatar>
+                    <p className="opacity-70 italic">{comment.author_name}:</p>
+                  </Link>
+                  <p>{comment.comment}</p>
+                </div>
+              );
+            })}
+          </div>
+        )
+      ) : null}
+      <form className="flex items-center gap-2" onSubmit={handleSubmit}>
+        <Input
+          placeholder="New comment"
+          onChange={(e) => setNewComment(e.target.value)}
+          value={newComment}
+        />
+        {newComment.trim() ? (
+          <Button variant={"ghost"} type="submit">
+            <PlusCircle />
+          </Button>
+        ) : null}
+      </form>
+    </div>
+  );
+};
+
 const Comments = ({ image }: { image: ImageType }) => {
   if (!image.isCommentable) {
     return (
@@ -29,76 +146,10 @@ const Comments = ({ image }: { image: ImageType }) => {
     );
   }
 
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [comments, setComments] = useState<Comment[] | Error>([]);
-  const [newComment, setNewComment] = useState<string>("");
-
-  useEffect(() => {
-    const fetchComments = async () => {
-      setIsLoading(true);
-      const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/comments/${image.id}`;
-      const res = await fetch(url, { cache: "no-store" });
-      const data = await res.json();
-      setComments(data);
-      setIsLoading(false);
-    };
-
-    fetchComments();
-  }, []);
-
   return (
     <div>
       <h1 className="text-xl font-semibold">Comments</h1>
-      <div className="flex flex-col gap-3">
-        {isLoading ? (
-          <div className="w-full h-[50px] animate-pulse bg-[#ffffff20]" />
-        ) : (
-          <div>
-            {"error" in comments ? (
-              <div>{comments.error}</div>
-            ) : Array.isArray(comments) ? (
-              comments.length === 0 && !isLoading ? (
-                <div className="text-gray-500">No comments yet, be first!</div>
-              ) : (
-                comments.map((comment: Comment) => {
-                  return (
-                    <div
-                      key={comment.image_uuid}
-                      className="flex flex-wrap gap-2"
-                    >
-                      <Link
-                        href={`/user/${comment.author_uuid}`}
-                        className="flex items-center gap-1"
-                      >
-                        <Avatar className="cursor-pointer border-[0.5px] border-[#ffffff40] size-5">
-                          <AvatarImage src={comment.author_avatar_url} />
-                          <AvatarFallback>
-                            <LoaderCircle className="animate-spin" />
-                          </AvatarFallback>
-                        </Avatar>
-                        <p className="opacity-70 italic">{comment.author_name}:</p>
-                      </Link>
-                      <p>{comment.comment}</p>
-                    </div>
-                  );
-                })
-              )
-            ) : null}
-          </div>
-        )}
-        <div className="flex items-center gap-2">
-          <Input
-            placeholder="New comment"
-            onChange={(e) => setNewComment(e.target.value)}
-            value={newComment}
-          />
-          {newComment ? (
-            <Button variant={"ghost"}>
-              <PlusCircle />
-            </Button>
-          ) : null}
-        </div>
-      </div>
+      <CommentsLogic image={image} />
     </div>
   );
 };
--- server/src/controllers/comments/add-comment.controller.js
+++ server/src/controllers/comments/add-comment.controller.js
@@ -3,6 +3,14 @@ import { supabase } from "../../config/supabase.js";
 const addComment = async (c) => {
   const { image_uuid, author_uuid, author_name, author_avatar_url, comment } =
     await c.req.json();
+  console.log({
+    image_uuid,
+    author_uuid,
+    author_name,
+    author_avatar_url,
+    comment,
+  });
+
   try {
     const { error } = await supabase.from("comments").insert([
       {
